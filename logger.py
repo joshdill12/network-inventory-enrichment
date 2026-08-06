a2b8686bc641ea5e70072bd139689343da9a9f99
Josh Dillon <joshdill12@gmail.com>
2026-08-06 16:56:16 -0500

    Added logger module

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+
+def setup_logger():
+    #Setting up applications logging.
+
+     logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
